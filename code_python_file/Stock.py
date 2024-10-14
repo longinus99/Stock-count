f803db6f6f5f0b3723d46c15fe52bdbf1ca26591
Gun Ho, Choi <54893409+longinus99@users.noreply.github.com>
2024-10-14 13:48:32 +0900

    Update Stock.py

--- code_python_file/Stock.py
+++ code_python_file/Stock.py
@@ -11,50 +11,46 @@ class ExcelLoaderApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        # 창의 타이틀 설정
         self.setWindowTitle('Stock')
 
         self.resize(800, 400)
 
-        # 레이아웃 설정
+    
         layout = QVBoxLayout()
 
-        # 파일 로드 버튼 생성
+    
         self.load_button = QPushButton('재고 불러오기', self)
         self.load_button.clicked.connect(self.load_excel_file)
         layout.addWidget(self.load_button)
 
-        # 파일 삭제 버튼 생성
         self.delete_button = QPushButton('파일 삭제하기', self)
         self.delete_button.clicked.connect(self.delete_excel_file)
         layout.addWidget(self.delete_button)
 
-        # 결과 출력용 텍스트박스 생성
+       
         self.result_text = QTextEdit(self)
-        self.result_text.setReadOnly(True)  # 읽기 전용으로 설정
+        self.result_text.setReadOnly(True) 
         layout.addWidget(self.result_text)
 
-        # 레이아웃을 메인 윈도우에 설정
+
         self.setLayout(layout)
 
     def load_excel_file(self):
-        # 디렉토리 설정 (현재 디렉토리)
         directory = './'
         folder_path = './상품'
         self.result_text.clear()
 
-        # 디렉토리에서 파일을 찾는 부분
         for filename in os.listdir(directory):
             if filename.startswith("현재창고관리") and filename.endswith(".xls"):
                 self.file_path = os.path.join(directory, filename)
                 
-                # 결과 출력
+            
                 # self.result_text.append(f"불러오는 파일: {file_path}")
                 
-                # Excel 파일을 Pandas DataFrame으로 불러오기
+           
                 df = pd.read_excel(self.file_path)
 
-                # 데이터프레임의 내용을 표시
+           
                 # self.result_text.append(f"DataFrame 정보:\n{df.head()}")
                 break
         
@@ -63,16 +59,15 @@ class ExcelLoaderApp(QWidget):
             new_df = df[['상품분류', '상품명','현재창고재고']]
 
             for filename in os.listdir(folder_path):
-                if filename.endswith('.txt'):  # 텍스트 파일만 선택
+                if filename.endswith('.txt'):  
                     cupname = os.path.splitext(filename)[0]
 
-                    product_names = []  # 각 파일에 대한 상품명 리스트 초기화
+                    product_names = [] 
                     with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as file:
                         # 각 파일의 상품명을 읽어서 리스트에 추가
                         for line in file:
                             product_names.append(line.strip())
 
-                    # 4. 재고 값 합산
                     total_stock = 0
 
                     for product in product_names:
@@ -85,7 +80,7 @@ class ExcelLoaderApp(QWidget):
                         else:
                             self.result_text.append(f'{product}는 재고에 없습니다!')
 
-                    # 결과 출력
+           
                     self.result_text.append(f'{os.path.splitext(filename)[0]}: {total_stock}')
         except:
             self.result_text.clear()
@@ -93,14 +88,12 @@ class ExcelLoaderApp(QWidget):
 
     def delete_excel_file(self):
             if self.file_path:
-                # 사용자에게 삭제 여부를 확인하는 메시지 박스
                 reply = QMessageBox.question(self, '파일 삭제 확인', 
                                             f'{self.file_path} 파일을 삭제하시겠습니까?', 
                                             QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
 
                 if reply == QMessageBox.Yes:
                     try:
-                        # 파일 삭제
                         os.remove(self.file_path)
                         # self.result_text.append(f"{self.file_path} 파일이 삭제되었습니다.")
                         self.file_path = None  # 파일 경로를 None으로 초기화
